36899d7551352425a7bee0772eb0c76e02c1e106
Sohel Shaikh <rebht78@gmail.com>
2018-10-12 20:52:38 +0530

    You will get your first error in python when running exercise3

---
+++ exercise3.py
@@ -0,0 +1,11 @@
+"""
+Let's prove our postulate 1 discussed in exercise 2.
+"""
+# printing the first line just for print sake
+print('Hey')
+
+# it will result into error
+print("hi, i am normal but with two " quotation ")
+
+# what error you get?
+# Read the error carefully and head over to exercise4.py to understand it.
