5b8b83114c0f688cfb710924cdc153a5280feff8
Sohel Shaikh <rebht78@gmail.com>
2018-10-12 20:44:51 +0530

    Added another exercise for printing values

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+"""
+Let us try some more values for printing purpose.
+
+Again, print is a function in python to display output on screen.
+
+You need to provide values in ()
+"""
+print(2)
+print(2.34) # I am a comment.
+print('String in single quotes') # Python ignore pound or hash sign
+
+# you will find me in other programs as well.
+# to explain some other concepts.
+
+print("String in double quotes")
+
+# String in python can be in ' ' or "" quotations. 
