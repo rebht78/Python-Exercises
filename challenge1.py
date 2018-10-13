2570e3c858d05653094ed1f13caac906baaa9e07
Sohel Shaikh <rebht78@gmail.com>
2018-10-13 18:00:11 +0530

    Challenge 1 Added

---
+++ challenge1.py
@@ -0,0 +1,14 @@
+"""
+You have learn so much about programming.
+
+Let's challenge ourselves in this cool challenge.
+
+Challenge 1:
+Try to print - "I am learning Python and it is awesome!" using Python
+
+Challenge 2:
+Determine the type of '2.14' (single quotation included) and 2.14 using 
+Python. Are they same or different? Why?
+
+Answers to these questions will be posted in exercise6.py
+"""
