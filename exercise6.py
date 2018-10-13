940ce5eb068e09bff1a7280ea23fd01de22a4e81
Sohel Shaikh <rebht78@gmail.com>
2018-10-13 20:09:36 +0530

    Added answers to challenge1

---
+++ exercise6.py
@@ -0,0 +1,15 @@
+"""
+Answers to challenges
+"""
+# answer to challenge 1
+# this was pretty easy - you just need to use print() function
+print("I am learning Python and it is awesome!")
+
+# answer to challenge 2
+# this was a confusing question
+# to determine type of any value use type() function
+print(type('2.14')) # <type 'str'>
+print(type(2.14))  # <type 'float'>
+
+# why are they different: because whatever is in quotation, python
+# assumes it as string.
