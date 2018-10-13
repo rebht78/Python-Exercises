f6e5d10c4a12531cc3cbb3193fae3d62bce7d12d
Sohel Shaikh <rebht78@gmail.com>
2018-10-13 17:56:47 +0530

    Added exercise5 that explains various types in python

---
+++ exercise5.py
@@ -0,0 +1,29 @@
+"""
+In all the previous programs, we try to print something 
+on the screen.
+
+Starting with 'Hello World' to a number.
+
+Let's now move forward with types in python.
+
+Python has three basic data types.
+
+Data Types help python in determining type of operations
+you can perform with those data.
+
+You can find out the type of any data using type() function
+We'll understand what is the meaning of function in subsequent exercises.
+"""
+
+# print a number and it's type
+print(20)
+print(type(20))
+
+#print a real number [with a decimal] and it's type
+print(3.14)
+print(type(3.14))
+
+#print a string and it's type
+print('Hello, World') 
+# anything in single or double quotation is a string
+print(type('Hello, World'))
