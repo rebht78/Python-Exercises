7461447ca25efde9a6200bb66a7b732ebb2f5beb
Sohel Shaikh <rebht78@gmail.com>
2018-10-15 23:03:42 +0530

    Added exercise 9 that shows operator

---
+++ exercise8.py
@@ -0,0 +1,18 @@
+"""
+Let's learn some operators.
+
+Operators are symbols that have special meaning for Python.
+
+As for example: +, -, *, /, %, **
+
+Let's see some examples
+"""
+number1 = 32 # a variable number1 with value 32
+number2 = 45 # a variable number2 with value 45
+
+sum = number1 + number2 # a sum variable that add two numbers using +
+
+# you can name variables according to your desire.
+# but it should be relevant
+
+print(sum)
