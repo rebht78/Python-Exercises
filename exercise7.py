1b93c1c96152a0bc131393af4cbeeb9bf5e0a5d3
Sohel Shaikh <rebht78@gmail.com>
2018-10-15 22:58:44 +0530

    Added exercise7 for variables

---
+++ exercise7.py
@@ -0,0 +1,22 @@
+"""
+In this set of exercises, we'll learn about variables.
+
+Variables are named memory location.
+
+Instead of referring values by values why not to create variables.
+We create variables simply by giving them the value
+
+<varname> = <value> 
+
+that's it
+"""
+name = 'Sherlock' # created a variable name
+
+# let's print it
+print(name);
+
+age = 32 # created a variable age with 32 as value
+
+# let's print it
+print(age);
+
