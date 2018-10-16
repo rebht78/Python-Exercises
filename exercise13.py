801867001b3ab218d83f4a50e89419b7f21a5d0e
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:21:06 +0530

    added exercise13 for various comparison operators

---
+++ exercise13.py
@@ -0,0 +1,15 @@
+"""
+Other comparison operators: <, >=, <=, ==, !=
+
+we will see equality check (==) and not equality check (!=) 
+"""
+number1 = 30
+number2 = 10
+
+equality = number1 == number2 
+print(equality)
+
+notEquality = number1 != number2
+print(notEquality)
+
+# change some values to change the output.
