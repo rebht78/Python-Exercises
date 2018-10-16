b86d828ee2016b1d3ae0df2926c9fdbbd4936366
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:14:56 +0530

    Added exercise11 for boolean value

---
+++ exercise11.py
@@ -0,0 +1,20 @@
+"""
+It's the time to introduce another type in Python: Boolean
+
+Boolean or bool is a type that only takes two value: True or False.
+
+Let's see bool in action.
+"""
+
+# you are now acquainted with variables
+number1 = 30
+number2 = 10
+
+result = number1 > number2 
+# > (greater than) is a comparison operator
+# that compares two values and return True or False
+# change values to get False value
+
+# result = 30 > 10
+# result = True
+print(result)
