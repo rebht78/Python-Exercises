90aa70c31cad589fa76037447fbaab4858d86ba9
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:17:29 +0530

    added exercise12 to display type of boolean variables

---
+++ exercise12.py
@@ -0,0 +1,20 @@
+"""
+Recap:
+Boolean or bool is a type that only takes two value: True or False.
+
+Let's see the type of the result variable from previous example.
+"""
+
+# you are now acquainted with variables
+number1 = 30
+number2 = 10
+
+result = number1 > number2 
+# > (greater than) is a comparison operator
+# that compares two values and return True or False
+# change values to get False value
+
+# result = 30 > 10
+# result = True
+print(result)
+print(type(result))
