479c2c38bdb4346fda7e07944131327d44f63384
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:10:18 +0530

    added exercise10 for division operators

---
+++ exercise10.py
@@ -0,0 +1,18 @@
+"""
+Let's see some others operators.
+
+Examples of / and %
+"""
+number1 = 30 # a variable number1 with value 30
+number2 = 4 # a variable number2 with value 4
+
+quotient = number1/number2 # dividing number 1 from number 2
+# quotient = 30 / 4
+# quotient = 7 
+print(quotient)
+
+rem = number1 % number2 # dividing number 1 from number 2 
+# and give remainder
+# mul = 30 % 4
+# mul = 2
+print(rem)
