cd06b7869e9916ef90aae4c32b4fb5fcb85af014
Sohel Shaikh <rebht78@gmail.com>
2018-10-16 22:05:41 +0530

    Added other examples of operators in exercise9

---
+++ exercise9.py
@@ -0,0 +1,23 @@
+"""
+Let's learn some others operators.
+
+Let's see some examples of -, *, **
+"""
+number1 = 3 # a variable number1 with value 3
+number2 = 4 # a variable number2 with value 4
+
+sub = number2 - number1 # subtracting number 2 from number 1
+# sub = 4 -3
+# sub = 1
+print(sub)
+
+mul = number1 * number2 # multiply number1 with number2
+# mul = 3 * 4
+# mul = 12
+print(mul)
+
+# ** is a special operator in python
+# x ** y (x raise to y)
+# 2 ** 3 (2 raise to 3 => 8)
+exp = number1 ** number2
+print(exp)
