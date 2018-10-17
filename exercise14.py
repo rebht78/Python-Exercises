ac306ab3e4b9167d12558638a4b98b11b7c24422
Sohel Shaikh <rebht78@gmail.com>
2018-10-17 20:24:33 +0530

    added exercise14 for other comparison operators

---
+++ exercise14.py
@@ -0,0 +1,19 @@
+"""
+Other comparison operators: <, >=, <=, ==, !=
+
+we will see >= and <= 
+"""
+number1 = 30
+number2 = 30
+
+result = number1 >= number2 
+# check whether number1 is greater than or equal to number2
+# result = 30 >= 30
+# as 30 is equal to 30, we'll get True
+
+# if number1 is 50, number2 is 30, then also above expression
+# will be True
+ 
+print(result)
+
+# change values to change the output.
