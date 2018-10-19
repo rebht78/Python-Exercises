8656354279858f7871153e4c76ef78c959616f76
Sohel Shaikh <rebht78@gmail.com>
2018-10-19 21:54:28 +0530

    Added explanation of str in python

---
+++ exercise19.py
@@ -0,0 +1,31 @@
+"""
+In the previous exercise We got introducted to str.
+
+Previously, we've use type to get the type of 'value'.
+
+type, str, print and so many others are built-in functions
+in python.
+
+str is a function that converts non-string value to string value.
+
+Let us see an example of it.
+"""
+number1 = 23
+number2 = 32
+sum = number1 + number2
+
+# this will print 55
+print(sum)
+
+# let us change the values
+name = 'Sherlock'
+age = 32
+
+# let us try to add name and age
+# Are you kidding?
+# you cannot add characters and numbers
+# let's try
+sumOfNameAge = name + age
+print(sumOfNameAge)
+
+# what you got?
