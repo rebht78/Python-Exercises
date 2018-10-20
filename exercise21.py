c9fcbe255330008854beee5515287efe13a2f8fd
Sohel Shaikh <rebht78@gmail.com>
2018-10-20 22:10:00 +0530

    added exercise to explain need of loop!

---
+++ exercise21.py
@@ -0,0 +1,23 @@
+"""
+In these set of exercises - from 21 to 23, we're going to learn about
+Loops.
+
+Loops is a block of statements that get repeatedly executed
+until the condition is true.
+
+But why we need loop?
+
+Let's see example of it.
+"""
+
+# let's say we need to print 1 to 5
+# we can print it explicitly
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5) # thank God, it's over!
+
+# it was quite tedious, right?
+# head over to exercise22 to see another alternative.
