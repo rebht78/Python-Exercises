106a890ae68697f37b019932e1b1cc5e65c206bf
Sohel Shaikh <rebht78@gmail.com>
2018-10-22 21:53:17 +0530

    Added exercise25 to explain index of list in python

---
+++ exercise25.py
@@ -0,0 +1,17 @@
+"""
+Syntax of list creation again:
+
+listname = [value1,value2,...]
+
+we can print the whole list but what if I want to access one 
+of the element of the list.
+
+You can use index. Index starts from 0 in python.
+so, first element is at index 0, last element is at index n-1
+where n is the size of the list.
+"""
+countries = ['USA','Canada','India','Russia','United Kingdom'];
+# we have just created the list
+
+print(countries[0]) # print the first element
+print(countries[2]) # print the third element
