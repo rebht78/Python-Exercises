a2a9c2d6d870226e5e13ac49eaa801f971359c40
Sohel Shaikh <rebht78@gmail.com>
2018-10-22 21:49:09 +0530

    Added exercise24 to explain list in python

---
+++ exercise24.py
@@ -0,0 +1,29 @@
+"""
+We have learn a lot about python.
+
+Now let's move forward and learn Sequences or Lists.
+
+Lists are collection of values.
+
+Why we need a list in first place?
+
+Let's say you need to store name of all the countries in the world
+to store their GDP numbers.
+You can go on and create 190+ variables for storing country name
+or create a list of countries.
+
+Let's see few examples of it.
+
+Syntax of list creation:
+
+listname = [value1,value2,...]
+
+"""
+countries = ['USA','Canada','India','Russia','United Kingdom'];
+# we have just created the list
+
+# you can print the list too.
+print(countries)
+
+
+
