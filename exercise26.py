b96daffdaa08315536ad671b6fce1ede7e4183ed
Sohel Shaikh <rebht78@gmail.com>
2018-10-23 21:55:56 +0530

    Added exercise26 to explain negative index of list in python

---
+++ exercise26.py
@@ -0,0 +1,16 @@
+"""
+We can access individual elements using index.
+
+Suppose a list name is countries.
+
+we can access elements like countries[index]
+
+index starts from 0 to n-1 where n is the size of list.
+
+Question is 
+"Can we use negative index to list?"
+"""
+countries = ['USA','Canada','India','Russia','United Kingdom'];
+
+print(countries[-1]) # print the last element
+print(countries[-3]) # print the third last element
