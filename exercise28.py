17403db3ca2eac001370188bd5fb1bd01ed8aec2
Sohel Shaikh <rebht78@gmail.com>
2018-10-23 22:02:07 +0530

    Added exercise28 to explain for loop syntax in python

---
+++ exercise28.py
@@ -0,0 +1,21 @@
+"""
+It is the time to introduce another loop specifically for lists.
+
+Name of the loop is for
+
+Syntax is :
+for varname in listname:
+	# you can do whatever you want to
+	# here body of the loop will be
+
+This loop will process each element of the list until there are no more 
+elements
+
+Let's see an example
+"""
+countries = ['USA','Canada','India','Russia','United Kingdom'];
+# now, you are acquainted with list syntax
+
+for country in countries:
+	print(country) # I am just printing the elements here
+	# all the elements
