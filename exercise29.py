7107e7077ad86db6bf3037d3ef033de7a6833d81
Sohel Shaikh <rebht78@gmail.com>
2018-10-24 21:46:55 +0530

    Added exercise29 to explain string slicing

---
+++ exercise29.py
@@ -0,0 +1,19 @@
+"""
+We are used to list now, let's introduce some more functions to list.
+
+So moving further,
+String is also a kind of list. Because String is a collection of 
+characters.
+
+If string in python is a collection of characters (list), then 
+we can use all the techniques of list with strings too.
+
+Let's see an example of all those techniques using string.
+"""
+text = "Elementary, My Dear Watson"
+# sherlockian quotes
+
+print(text[0])
+# print first character
+print(text[0:10])
+# using range with text (actually it is called slicing)
