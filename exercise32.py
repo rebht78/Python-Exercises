34694d33422016a7140f58c6ce967f5c191a5dba
Sohel Shaikh <rebht78@gmail.com>
2018-10-25 21:50:17 +0530

    Added exercise32 that explains python function with arguments

---
+++ exercise32.py
@@ -0,0 +1,27 @@
+"""
+Introducing Arguments:
+
+Earlier functions were blocks of code but they were not dynamic.
+I was not able to pass values to the function.
+
+printFiveTimes was only able to print 'Sherlock'. 
+If I want to print some other value what I need to do.
+
+Do I need to change function block everytime?
+
+Better solution is to pass arguments.
+
+Let's see the updated printFiveTimes with argument
+"""
+def printFiveTimes(data):
+	print(data*5) # we're using data within function
+
+# calling printFiveTimes without any value
+# you'll get an error just try it once.
+# I am passing 'Watson' here.
+printFiveTimes('Watson')
+
+name = 'Kim'
+printFiveTimes(name) # you can also pass variables
+# but pass string only.
+
