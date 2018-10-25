7c18a77ae55b07094a98a0f457207375d6721c6d
Sohel Shaikh <rebht78@gmail.com>
2018-10-25 21:46:30 +0530

    Added exercise31 that explains python function

---
+++ exercise31.py
@@ -0,0 +1,12 @@
+"""
+Let's create another function that is more complex
+than earlier we have created.
+"""
+def printFiveTimes():
+	text = "Sherlock"
+	print(text*5) # yes We're multiplying string with 5
+	# it will print text five times
+	# Let's see by calling it
+
+# calling printFiveTimes
+printFiveTimes()
