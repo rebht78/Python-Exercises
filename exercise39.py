5b9578b6e685c80ece1d532935d0ee0b312ecfc2
Sohel Shaikh <rebht78@gmail.com>
2018-10-29 22:20:25 +0530

    Added exercise39 as an example of range function

---
+++ exercise39.py
@@ -0,0 +1,16 @@
+"""
+In this exercise, I'll introduce to you range() function.
+
+range() function is the most important function in python
+and one of the most useful in data science.
+
+range syntax: range(start,end[not inclusive])
+
+You can generate numbers using range().
+
+"""
+# let's use range() function in print
+print(range(0,9))
+# you'll get the output in the form of a list
+# starting from 0 to 8.
+
