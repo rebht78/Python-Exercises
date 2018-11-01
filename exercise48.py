6fb8fa402470945b93141bb6e8f99e7048928ed1
Sohel Shaikh <rebht78@gmail.com>
2018-11-01 21:56:02 +0530

    added exercise48 to explain list multiplication

---
+++ exercise48.py
@@ -0,0 +1,11 @@
+"""
+In this exercise, we'll see another operator : *
+
+Let's see an example.
+"""
+list1 = ['a','b','c',1,2,3]
+
+list2 = list1 * 2 # assume an answer
+
+print(list2)
+# printing list2
