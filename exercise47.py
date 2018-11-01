18ab8b69873171f8a8e33d0b9783f0b0b2e3ef51
Sohel Shaikh <rebht78@gmail.com>
2018-11-01 21:54:10 +0530

    added exercise47 to explain list concatenation

---
+++ exercise47.py
@@ -0,0 +1,16 @@
+"""
+In this and subsequent exercises, we'll learn more about lists.
+
+Let's see an example.
+"""
+list1 = ['a','b','c',1,2]
+# creation of list1
+
+list2 = [3,4,5,6]
+# creation of list2
+
+# let's create list3 by adding list1 and list2
+list3 = list1 + list2
+# before printing the list, assume an answer
+
+print(list3)
