ec01d6a06fa282658adb71c9656882d726e21f4b
Sohel Shaikh <rebht78@gmail.com>
2018-11-12 21:41:19 +0530

    added exercise50 to explain sort() method

---
+++ exercise50.py
@@ -0,0 +1,15 @@
+"""
+2. sort() : arranges the element of the list in increasing order.
+
+Let's see an example of sort() method.
+"""
+fav_fruits = ['apples','oranges','pears','bananas','pineapples']
+
+# printing favorite fruits
+print(fav_fruits)
+
+# using sort to arrange the elements
+fav_fruits.sort()
+
+# printing the sorted list
+print(fav_fruits)
