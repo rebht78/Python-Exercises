388323f7493eb5b144fdd68a1c791130f7fbe616
Sohel Shaikh <rebht78@gmail.com>
2018-11-12 21:40:50 +0530

    added exercise49 to explain append() method

---
+++ exercise49.py
@@ -0,0 +1,18 @@
+"""
+In this and subsequent exercises, we'll see
+list methods.
+
+1. append() : append is use to add new elements at the end 
+of the list.
+
+Let's see an example of append() method.
+"""
+list1 = ['a','b','c',1,2,3]
+
+# printing the list
+print(list1)
+# adding new elements: orange to list1
+list1.append('orange')
+
+# printing the new list
+print(list1)
