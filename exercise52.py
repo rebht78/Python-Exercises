407a56b4675cebd11f2973d86f732478b0833164
Sohel Shaikh <rebht78@gmail.com>
2018-11-13 21:40:44 +0530

    added exercise52 to explain remove example

---
+++ exercise52.py
@@ -0,0 +1,16 @@
+"""
+4. remove() : is used to delete elements from the list, if you don't know
+the index of the element
+
+Let's see an example of remove() method.
+"""
+fav_fruits = ['apples','oranges','pears','bananas','pineapples']
+
+# printing favorite fruits
+print(fav_fruits)
+
+# removing pears from fav_fruits list
+fav_fruits.remove('pears')
+
+# printing the sorted list
+print(fav_fruits)
