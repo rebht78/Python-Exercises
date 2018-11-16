59b21289167a85a98f8fae345ba58ce93152996c
Sohel Shaikh <rebht78@gmail.com>
2018-11-16 21:29:02 +0530

    added exercise54 to explain list and string

---
+++ exercise54.py
@@ -0,0 +1,14 @@
+"""
+A string is a sequence of characters
+and list is a sequence of values. 
+
+But list of characters is not same as string.
+
+Let's see few examples related to list and string
+"""
+str1 = "This is a long string"
+
+list1 = list(str1) # list is to use to convert string to list
+
+print(str1)
+print(list1) # you can see the difference.
