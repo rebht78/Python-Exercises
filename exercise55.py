f49986dd9f76bd15ebc1c36e9fba5c018cba1b42
Sohel Shaikh <rebht78@gmail.com>
2018-11-16 21:30:29 +0530

    added exercise55 to explain list and string

---
+++ exercise55.py
@@ -0,0 +1,11 @@
+"""
+Let's see another example of list and string
+where we'll split string into list
+"""
+str1 = "This is a long string"
+
+# let's create a list through space
+list1 = str1.split()
+
+print(str1)
+print(list1) # you can see the difference.
