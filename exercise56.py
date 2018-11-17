be4a26035a73d9aab9e1e3c4b139376748771944
Sohel Shaikh <rebht78@gmail.com>
2018-11-17 21:45:05 +0530

    added exercise56 to explain list and string with split

---
+++ exercise56.py
@@ -0,0 +1,12 @@
+"""
+Let's see another example of split with arguments
+where we'll split string into list using commas
+"""
+str1 = "This,is,a,long,string"
+
+# let's create a list through space
+list1 = str1.split(',')
+
+print(str1)
+print(list1) # elements will be separated according to 
+# comma
