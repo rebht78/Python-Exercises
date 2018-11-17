e2c5924f8f8591c8bfc16cc93f801b28bb422466
Sohel Shaikh <rebht78@gmail.com>
2018-11-17 21:50:08 +0530

    added exercise57 to explain simple dictionary

---
+++ exercise57.py
@@ -0,0 +1,16 @@
+"""
+A dictionary is just like a list, instead of indices we have keys.
+
+let's see a simple example of dictionary.
+"""
+eng2span = {
+	'one':'uno',
+	'two':'dos',
+	'three':'tres',
+	'lunes':'monday',
+	'domingos':'sunday'
+}
+
+print(eng2span)
+
+print('The value of one in spanish is '+str(eng2span['one']))
